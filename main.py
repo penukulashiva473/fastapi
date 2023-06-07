a09a045b9d3554feabcff1295ca3accfbcdb3956
shiva <shiva@jaguarsoftwareindia.com>
2023-06-07 19:07:47 +0530

    minor

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: str):
     return {"item_id": item_id, "q": q}
